7910451c38d871c6fb7e3f2dc0e8a4f395914e51
Barry <369916324@qq.com>
2017-10-12 16:33:01 +0800

    commit singerList

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -6,8 +6,12 @@
 </template>
 
 <script type="text/ecmascript-6">
+    import Singer from 'common/js/singer';
     import {getSingerList} from 'api/singer';
     import {ERR_OK} from 'api/config';
+
+    const HOT_NAME = '热门';
+    const HOT_SINGER_LEN = 10;
 
     export default {
         data(){
@@ -19,14 +23,44 @@
             this._getSingerList();
         },
         methods: {
-            _getSingerList() {
+            _getSingerList() {  // 获取歌手数据
                 getSingerList().then((res) => {
                     if (res.code === ERR_OK) {
                         this.singers = res.data.list;
-                        console.log(this.singers);
+                        console.log(this._normalizeSinger(this.singers));
                     }
-                })
+                });
             },
+            _normalizeSinger(list){  // 规范歌手数据
+                let map = {  // 创建对象
+                    hot: {
+                        title: HOT_NAME,
+                        items: []  // 内层数组
+                    }
+                }
+                list.forEach((item, index) => {  // 遍历list
+                    if (index < HOT_SINGER_LEN) {  // 默认10条
+                        map.hot.items.push(new Singer({  // 热门数据填充items
+                            id: item.Fsinger_mid,
+                            name: item.Fsinger_name
+                        }));
+                    }
+                    const key = item.Findex;  // Findex歌手首字母
+                    if (!map[key]) {  // 如果没有以这个key为聚合的对象的话 那就创建一个
+                        map[key] = {
+                            title: key,
+                            items: []
+                        }
+                    }
+                    map[key].items.push(new Singer({  // 给map[key].items填充数据
+                            id: item.Fsinger_mid,
+                            name: item.Fsinger_name
+                        }
+                    ));
+                });
+                // 因为遍历对象得到的结果是无序的 为了得到有序列表 我们要处理给map排序
+
+            }
         }
     }
 </script>
